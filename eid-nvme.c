afa8538dcf45dbe9e57a3a8c109d9dfc8ebc4527
Andrew Maier <Andrew.Maier@eideticom.com>
2017-11-15 13:13:23 -0800

    Added the human readable option and finished parsing the accelerator specific information

--- eid-nvme.c
+++ eid-nvme.c
@@ -49,7 +49,7 @@ struct eid_noload {
 	char             hls[12];
 	char             acc_name[64];
 	__le32           acc_ver;
-	char             acc_cfg[24];
+	__le32           acc_cfg[6];
 	__le32           acc_priv_len;
 	char             acc_priv[3600];
 };
@@ -68,8 +68,8 @@ static void eid_print_list_item(struct list_item list_item)
 		(struct eid_noload *) &list_item.ns.vs;
 	
 	printf("%-16s %-64.64s %-8.8u 0x%-8.8x\n", list_item.node,
-	       eid->acc_name, (unsigned int) eid->acc_ver,
-	       (unsigned int) eid->acc_status);
+				 eid->acc_name, (unsigned int) eid->acc_ver,
+				 (unsigned int) eid->acc_status);
 }
 
 static void eid_print_list_items(struct list_item *list_items, unsigned len)
@@ -77,28 +77,89 @@ static void eid_print_list_items(struct list_item *list_items, unsigned len)
 	unsigned i;
 
 	printf("%-16s %-64s %-8s %-10s\n",
-	       "Node", "Accelerator Name", "Version", "Status");
+				 "Node", "Accelerator Name", "Version", "Status");
 	printf("%-16s %-64s %-8s %-10s\n",
-	       "----------------",
-	       "----------------------------------------------------------------",
-	       "--------", "----------");
+				 "----------------",
+				 "----------------------------------------------------------------",
+				 "--------", "----------");
 	for (i = 0 ; i < len ; i++)
 		eid_print_list_item(list_items[i]);
 }
 
-static void eid_show_nvme_id_ns(struct nvme_id_ns *ns)
+static void eid_show_nvme_id_ns_status(__le32 status)
 {
+	__u16 upper_rsvd = (status & 0xFFFE0000) >> 17;
+	__u16 lower_rsvd = (status & 0x000000F8) >> 3;
+	int as_en = status & 0x1; // Bit 0 boolean
+	int as_rdrdy = (status & 0x2) >> 1; // Bit 1 boolean
+	int as_wrrdy = (status & 0x4) >> 2; // Bit 2 boolean 
+	__u8 as_err = (status & 0xFF00) >> 8; // Bits 15:8
+	int as_async = (status & 0x10000) >> 16; // Bit 16 boolean
+
+	if (upper_rsvd)
+		printf("\t[31:17]\t: 0x%x\tReserved\n", upper_rsvd);
+
+	printf("\t[16:16]\t: %#x\tAsync Event Request ", as_async);
+	if(as_async)
+		printf("enabled ");
+	else
+		printf("disabled ");
+	printf("(AS.ASYNC)\n");
+
+	if(as_err)
+		printf("\t[15:8]\t: %d\tError 0x%x occurred ", as_err, as_err);
+	else
+		printf("\t[15:8]\t: %d\tNo errors have been reported ", as_err);
+	printf("(AS.ERR)\n");
+
+	if(lower_rsvd)
+		printf("\t[7:3]\t: 0x%x\tReserved\n", lower_rsvd);
+
+	if(as_wrrdy)
+		printf("\t[2:2]\t: %d\tAccelerator is ready for the next write command ", as_wrrdy);
+	else
+		printf("\t[2:2]\t: %d\tAccelerator is NOT ready for the next write command ", as_wrrdy);
+	printf("(AS.WRRDY)\n");
+
+	if(as_rdrdy)
+		printf("\t[1:1]\t: %d\tAccelerator is ready for the next read command ", as_rdrdy);
+	else
+		printf("\t[1:1]\t: %d\tAccelerator is NOT ready for the next read command ", as_rdrdy);
+	printf("(AS.RDRDY)\n");
+
+	if(as_en)
+		printf("\t[0:0]\t: %d\tAccelerator is enabled ", as_en);
+	else
+		printf("\t[0:0]\t: %d\tAccelerator is NOT enabled ", as_en);
+	printf("(AS.EN)\n\n");
+}
+
+static void eid_show_nvme_id_ns(struct nvme_id_ns *ns, unsigned int mode)
+{
+	unsigned i;
+	int human = mode & HUMAN;
 	struct eid_noload *eid =
 		(struct eid_noload *) &ns->vs;
 
-	printf("acc_status : 0x%-8.8x\n", eid->acc_status);
-	printf("acc_name   : %s\n", eid->acc_name);
+	printf("acc_name\t: %s\n", eid->acc_name);
+	printf("acc_status\t: 0x%-8.8x\n", eid->acc_status);
+	if (human)
+		eid_show_nvme_id_ns_status(eid->acc_status);
+	printf("acc_version\t: %-8.8d\n", eid->acc_ver);
+	for(i = 0; i < 24/4; ++i)
+		printf("acc_cfg[%d]\t: 0x%-8.8x\n", i, eid->acc_cfg[i]);
+	printf("acc_spec_bytes\t: %d\n", eid->acc_priv_len);
+	if(eid->acc_priv_len)
+	{
+		printf("acc_user_space\t:\n");
+		d((unsigned char *)eid->acc_priv, eid->acc_priv_len, 16, 1);
+	}
+
 //	char             hls[12];
 //	char             acc_name[64];
 //	__le32           acc_ver;
 //	char             acc_cfg[24];
 //	__le32           acc_priv_len;
-
 }
 
 
@@ -109,7 +170,7 @@ static void eid_show_nvme_id_ns(struct nvme_id_ns *ns)
  */
 
 static int eid_list(int argc, char **argv, struct command *command,
-		    struct plugin *plugin)
+				struct plugin *plugin)
 {
 	char path[264];
 	struct dirent **devices;
@@ -175,19 +236,22 @@ static int eid_list(int argc, char **argv, struct command *command,
 }
 
 static int eid_id_ns(int argc, char **argv, struct command *command,
-		     struct plugin *plugin)
+				 struct plugin *plugin)
 {
 	const char *desc = "Send an Identify Namespace command to the "\
-		"given device, returns properties of the specified namespace "\
-		"in human-readable format. Fails on non-Eideticom namespaces";
+		"given device, returns properties of the specified namespace. "\
+		"Fails on non-Eideticom namespaces.";
 	const char *namespace_id = "identifier of desired namespace";
+	const char *human_readable = "show infos in readable format";
 	struct nvme_id_ns ns;
 	struct stat nvme_stat;
+	unsigned int flags = 0;
 
 	int err, fd;
 
 	struct config {
 		__u32 namespace_id;
+		int human_readable;
 	};
 
 	struct config cfg = {
@@ -195,7 +259,8 @@ static int eid_id_ns(int argc, char **argv, struct command *command,
 	};
 
 	const struct argconfig_commandline_options command_line_options[] = {
-		{"namespace-id",    'n', "NUM", CFG_POSITIVE, &cfg.namespace_id,    required_argument, namespace_id},
+		{"namespace-id", 'n', "NUM", CFG_POSITIVE, &cfg.namespace_id, required_argument, namespace_id},
+		{"human-readable", 'H', "", CFG_NONE, &cfg.human_readable, no_argument, human_readable},
 		{NULL}
 	};
 
@@ -213,10 +278,13 @@ static int eid_id_ns(int argc, char **argv, struct command *command,
 		fprintf(stderr,
 			"Error: requesting namespace-id from non-block device\n");
 
+	if (cfg.human_readable)
+			flags |= HUMAN;
+
 	err = nvme_identify_ns(fd, cfg.namespace_id, 0, &ns);
 	if (!err) {
 		printf("NVME Identify Namespace %d:\n", cfg.namespace_id);
-		eid_show_nvme_id_ns(&ns);
+		eid_show_nvme_id_ns(&ns, flags);
 	}
 	else if (err > 0)
 		fprintf(stderr, "NVMe Status:%s(%x) NSID:%d\n",
