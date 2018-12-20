9cf05192b0534b9574478bedf0c37fbd6add8a20
Andrew Maier <Andrew.Maier@eideticom.com>
2018-12-20 16:14:27 -0700

    Fixed a compilation issue with the new folder layout

--- plugins/eideticom/eid-nvme.h
+++ plugins/eideticom/eid-nvme.h
@@ -1,5 +1,5 @@
 #undef CMD_INC_FILE
-#define CMD_INC_FILE eid-nvme
+#define CMD_INC_FILE plugins/eideticom/eid-nvme
 
 #if !defined(EIDETICOM_NVME) || defined(CMD_HEADER_MULTI_READ)
 #define EIDETICOM_NVME
